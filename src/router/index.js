1883b881c92401e235819716272b24210dcf620e
hsiehbau <834538068@qq.com>
2020-02-18 21:25:26 +0800

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Switch, Route, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Index from "../view/index/index";
 import Book from "../view/book/index";
@@ -8,21 +8,18 @@ import User from "../view/user/index";
 import Details from "../view/details/index";
 
 class RouterIndex extends Component {
-    render() {
-
-        return (
-            <Switch>
-                <Route path="/" exact render={()=>(
-                    <Redirect to="/index/all" />
-                )} />
-                 <Route path="/index/:id" component={Index}/>
-                <Route path="/book" component={Book}/>
-                <Route path="/about" component={About}/>
-                <Route path="/user/:id" component={User}/>
-                <Route path="/details/:id" component={Details}/>
-            </Switch>
-        )
-    }
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact render={() => <Redirect to="/index/all" />} />
+        <Route path="/index/:id" component={Index} />
+        <Route path="/book" component={Book} />
+        <Route path="/about" component={About} />
+        <Route path="/user/:id" component={User} />
+        <Route path="/details/:id" component={Details} />
+      </Switch>
+    );
+  }
 }
 
 export default RouterIndex;
